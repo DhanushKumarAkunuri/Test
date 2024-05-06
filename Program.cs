45d152d02faa0498bd261be9e3975eab5134413b
Dhanush <dhanushkumarakunuri1998@gmail.com>
2024-05-06 17:37:03 +0200

    created dotnet project and read from input text file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Test;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ReadInputFile("./input.txt");
+    }
+
+    static void ReadInputFile(string filePath)
+    {
+        foreach(var line in File.ReadLines(filePath))
+        {
+            Console.WriteLine($"{line}");
+        }
+    }
+}
